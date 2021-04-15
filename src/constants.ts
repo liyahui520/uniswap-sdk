d9cce6b2bf1cc54b97344379a25b3d52f9efb44d
yhli <liyahui360@163.com>
2021-04-15 22:59:18 +0800

    111

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x71a59ab44a2fabc56adb885c0011851ce90a09d5'
+
+export const INIT_CODE_HASH = '0xb9874a016ed6410b5154e02cb550df955b989884c5f34f3aeb438f3e237a3b6e'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
